914d95c2cecc07c14d069e02d76bbdf4213a98b7
Charles Van Norman <ccvannorman@gmail.com>
2018-01-09 12:14:13 -0600

    'added some vimeos'

--- static/js/vimeoPopup.js
+++ static/js/vimeoPopup.js
@@ -12,7 +12,6 @@ var VIMEO_OVERLAY = {
 		if (this.moving || !this.showing) return;
 		VIMEO_OVERLAY.moving = true;
 		$('#vimeoPopup').animate({ height : '0', width: '0'},500,function(){ 
-			console.log('finished closing!');
 			VIMEO_OVERLAY.moving = false;
 			VIMEO_OVERLAY.showing = false;  
 			$('#vimeoPopupContainer').hide();
@@ -23,7 +22,6 @@ var VIMEO_OVERLAY = {
 		if (this.moving || this.showing) return;
 		disableScroll();
 		VIMEO_OVERLAY.moving = true;
-		console.log('showing');
 //		$('#vimeo_iframe').attr('src',src+"?api=1");	
 		$('#vimeoPopupContainer').show();
 		$('#vimeoPopup').html("<iframe id='vimeo_iframe' src='"+src+"?title=1&amp;byline=1&amp;portrait=1&amp;autoplay=true'; frameborder='0'></iframe>"); // re-init the html for autoplay
@@ -31,10 +29,9 @@ var VIMEO_OVERLAY = {
 		h = w * 540 / 960;
 		$('#vimeoPopup iframe').width(w).height(h); 
 		$('#vimeoPopup').animate({ height: h+"px", width: w+'px'},1100,function(){ 
-			console.log('finished opening!');
 			VIMEO_OVERLAY.moving = false; 
 			VIMEO_OVERLAY.showing = true;
-			console.log('this.showing:'+VIMEO_OVERLAY.showing);
+			// console.log('this.showing:'+VIMEO_OVERLAY.showing);
 		});
 	}
 
